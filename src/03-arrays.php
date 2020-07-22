6a93876c7fdf4cca628ec7683979ff3d8a6cca38
vladv <vlad.vakalyk@gmail.com>
2020-07-22 21:23:24 +0300

    php lab array

--- src/03-arrays.php
+++ src/03-arrays.php
@@ -5,11 +5,18 @@
  * without changing the order.
  * Example: [1,3,2] => [1,3,3,3,2,2]
  *
- * @param  array  $input
+ * @param  array $input
  * @return array
  */
 function repeatArrayValues(array $input)
 {
+    $result = [];
+    foreach ($input as $key => $value) {
+        for ($i = 1; $i <= $value; $i++) {
+            $result[] = $value;
+        }
+    }
+    return $result;
 }
 
 /**
@@ -17,11 +24,23 @@ function repeatArrayValues(array $input)
  * Return the lowest unique value or 0 if there is no unique values or array is empty.
  * Example: [1, 2, 3, 2, 1, 5, 6] => 3
  *
- * @param  array  $input
+ * @param  array $input
  * @return int
  */
 function getUniqueValue(array $input)
 {
+    $array = array_count_values($input);
+    $array = array_filter($array, function ($value, $key) {
+        if ($value == 1) {
+            return true;
+        }
+    }, ARRAY_FILTER_USE_BOTH);
+    if (!$array) {
+        return 0;
+    }
+    $array = array_keys($array);
+    $result = min($array);
+    return $result;
 }
 
 /**
@@ -45,9 +64,24 @@ function getUniqueValue(array $input)
  *  'yellow' => ['orange', 'potato'],
  * ]
  *
- * @param  array  $input
+ * @param  array $input
  * @return array
  */
 function groupByTag(array $input)
 {
+    $tags = [];
+    foreach ($input as $value) {
+        $tags = array_merge_recursive($value['tags'], $tags);
+    }
+    $tags = array_unique($tags);
+    $resultArray = [];
+    foreach ($tags as $key => $value) {
+        foreach ($input as $key2 => $value2) {
+            if (in_array($value, $value2['tags'])) {
+                $resultArray[$value] = array_merge(isset($resultArray[$value]) ? $resultArray[$value] : [], [$value2['name']]);
+            }
+        }
+        sort($resultArray[$value], SORT_STRING);
+    }
+    return $resultArray;
 }
